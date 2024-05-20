18b5c15f96a758d40eabf6533f679faf43a48936
Skyler-code101 <zeroskyler32@gmail.com>
2024-05-20 05:21:21 -0500

    Update trademanager.lua

--- trademanager.lua
+++ trademanager.lua
@@ -1,40 +1,60 @@
-local dfpwm = require("cc.audio.dfpwm")
+
 local speaker = peripheral.find("speaker")
 
 local sp = peripheral.find("speaker")
+local tran = {}
+    
+local function existsinfile(playername,prime)
+    value = prime
+    repeat
+        value = value+1
+    until not fs.exists(playername,value)
+    return value
+    
+end
+local playername = ""
+term.setTextColor(colors.green)
+print("Customer Name")
+term.setTextColor(colors.lime)
+playername = read()
+handel = fs.open("Transactions/"..playername..tostring(existsinfile(playername,0))..".txt","w")
+term.setTextColor(colors.green)
+print("Enter Recived Items (Not Amount and Split by commas)")
+term.setTextColor(colors.lime)
 
-    local handel = fs.open("Transactions.txt","w")
-    if (read() == "y") then
-        local RecivedItems = ""
-        local AmountRI = ""
-        local Items = ""
-        local AmountI = ""
-        local playername = ""
-        print("Enter Recived Items (Not Amount and Split by commas)")
-        RecivedItems = read()
-        print(RecivedItems.."\n\nAmount Of Recived Items (Split By Commas and in order)")
-        AmountRI = read()
-        print(RecivedItems.."\n"..AmountRI.."\n\nItems Traded (The Item You Gave To the Customer and Split By Commas)")
-        Items = read()
-        print(RecivedItems.."\n"..AmountRI.."\n"..Items.."\n\nAmount Of Traded Items (Split By Commas and in order)")
-        AmountI = read()
-        print("Customer Name")
-        playername = read()
-        print("Complete the Transaction?")
-        if (read() == "y") then
-            print("Transaction Complete")
-            sp.playSound("entity.player.levelup",3.0)
-            handel.write("{\n"..playername.." Transaction\n"..RecivedItems.."\n"..AmountRI.."\n"..Items.."\n"..AmountI.."\n}")
-        elseif (read()=="n") then
-            print("Canceled")
-            sleep(.5)
-            os.shutdown()
-        else
-            os.reboot()
-        end
-    elseif (read() == "n") then
-        os.reboot()
-    else
-        os.reboot()
-    end
+tran.recivedItems = read()
+term.setTextColor(colors.blue)
+print("\n"..tran.recivedItems)
+term.setTextColor(colors.green)
+print("\n\nAmount Of Recived Items (Split By Commas and in order)")
+term.setTextColor(colors.lime)
+tran.amountRI = read()
+term.setTextColor(colors.blue)
+print("\n"..tran.recivedItems.."\n"..tran.amountRI)
+term.setTextColor(colors.green)
+print("\n\nItems Traded (The Item You Gave To the Customer and Split By Commas)")
+term.setTextColor(colors.lime)
+tran.items = read()
+term.setTextColor(colors.blue)
+print("\n"..tran.recivedItems.."\n"..tran.amountRI.."\n"..tran.items)
+term.setTextColor(colors.green)
+print("\n\nAmount Of Traded Items (Split By Commas and in order)")
+term.setTextColor(colors.lime)
+tran.amountI = read()
+term.setTextColor(colors.green)
+print("Complete the Transaction?")
+term.setTextColor(colors.lime)
+if (read() == "y") then
+    term.setTextColor(colors.yellow)
+    print("Transaction Complete")
+    sp.playSound("entity.player.levelup",3.0)
+    handel.write(textutils.serialise(tran))
+elseif (read()=="n") then
+    term.setTextColor(colors.red)
+    print("Canceled")
+    sleep(.5)
+    os.shutdown()
+else
+    os.reboot()
+end
 
