7b742c5944ad9a3e46e9d0b422266a379ca1bf0c
Skyler-code101 <zeroskyler32@gmail.com>
2024-05-31 00:35:13 -0500

    Create startup.lua

---
+++ Shop/startup.lua
@@ -0,0 +1,20 @@
+
+local function run()
+    while true do
+        shell.run("paymentTerm")
+        os.reboot()
+    end
+end
+local function awaitUnlock()
+    while true do
+        local e = os.pullEventRaw("disk")
+        if e == "disk" then
+            local fh = fs.open("disk/key","r")
+            if fh and fh.readAll() == password then
+                break
+            end
+        end
+    end
+end
+
+parallel.waitForAny(run,awaitUnlock)
