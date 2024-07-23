85a64319c49d429a364f3d9857b5e9c0da7fe167
Skyler-code101 <zeroskyler32@gmail.com>
2024-07-23 17:48:05 -0500

    ss

--- Stargate/stgdboot.lua
+++ Stargate/stgdboot.lua
@@ -1,11 +1,11 @@
 
 local file = http.get("https://github.com/Skyler-code101/CC-Items/raw/main/Stargate/file.lua").readAll()
-local versionpublic = textutils.unserialise(http.get("https://github.com/Skyler-code101/CC-Items/raw/main/Stargate/version"))
+local versionpublic = textutils.unserialise(http.get("https://github.com/Skyler-code101/CC-Items/raw/main/Stargate/version").readAll())
 
-local versionlocal = textutils.unserialise(fs.open("version","r").readAll())
+local versionlocal = textutils.unserialise(fs.open("version","r").readAll()) or { version = "0.0.0" }
 
 
-if versionpublic.version ~= versionlocal.version then
+if versionpublic.version ~= versionlocal.version and versionpublic.installable == true then
     local localfile = fs.open("link.lua","w")
     localfile.write(file)
     localfile.close()
